8413e881a815b14e094b0b0217e6845db65b028e
jpiccirillo <piccirilloj1@gmail.com>
2025-11-15 23:14:47 -0600

    add second example for pearson

--- src/components/continuous/PearsonCorrelationCoefficient/index.tsx
+++ src/components/continuous/PearsonCorrelationCoefficient/index.tsx
@@ -1,5 +1,6 @@
 export { default as PearsonExplanation } from "./explanation";
 
 import Example1 from "./examples/Example1";
+import Example2 from "./examples/Example2";
 
-export const PearsonExamples = [Example1];
+export const PearsonExamples = [Example1, Example2];
