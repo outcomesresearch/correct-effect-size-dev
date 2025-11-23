c22484fea538ec4d8796b28c076843c3227a4408
jpiccirillo <piccirilloj1@gmail.com>
2025-11-22 23:48:40 -0600

    Site metadata

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/optimal-figure/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/correct-effect-size-dev/" : "/",
 });
